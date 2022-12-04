1bc7a113136f02a1d9b14aafbb08ba1330db7c10
Vladislav Lavr <ewasince@gmail.com>
2022-12-04 16:33:37 +0300

    dockerized flask

---
+++ docker_config.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:VupsenPupsen228@postgres:5432/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = True
+    DEBUG = True
